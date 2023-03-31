44cabef1ba7b904138ca4192b66f842b938ee271
SaiJyothi D <saijyothid12@gmail.com>
2023-03-31 17:18:50 +0530

    Add project files.

---
+++ C#Pratice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.Write("Enter the number");
+            i=int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Enter the even number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Enter the odd number");
+                    
+                    Console.Read();
+                }
+            }
+
+        }
+    }
