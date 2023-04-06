798714dcecc6600be5b10351a625bc79f8c00c76
SaiJyothi D <saijyothid12@gmail.com>
2023-04-06 14:32:19 +0530

    fact

---
+++ Pratice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.WriteLine("Enter any number");
+            number=int.Parse(Console.ReadLine());
+            for(i=1;i<=number;i++)
+            {
+                fact=fact*i;
+            }
+            Console.WriteLine("Factorial of" + number + "is:" + fact);
+
+        }
+    }
+}
