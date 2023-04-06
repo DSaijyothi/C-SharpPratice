53e2c1149adbb19e2c46ac6733914c7bcbdb4cd4
SaiJyothi D <saijyothid12@gmail.com>
2023-04-06 18:15:25 +0530

    String Trim

---
+++ Pratice/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice
+{
+    internal class Class2
+    {
+        static void  Main(string[] args)
+        {
+            string myString = "       CSHARP ";
+            System.Console.WriteLine("The String Before Triming:" + "(" + myString + ")");
+            System.Console.WriteLine("The String  After Timing :"+"("+myString.Trim()+")");
+            Console.Read();
+        }
+    }
+}
